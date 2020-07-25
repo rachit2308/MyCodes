275c22ef0d7fd3390225187fca64d8f3ba35a2e9
RACHIT_PAVILION\Rachit <rachitgupta230801@gmail.com>
2020-07-25 21:24:27 +0530

    Ch-1

---
+++ Ques4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+float fahrenheit,celsius;
+
+printf("Enter the Fahrenheit temp= ");
+scanf("%f",&fahrenheit);
+
+celsius=5.0/9.0*(fahrenheit-32);
+printf("Temperature in C= %f",celsius);
+}
+
