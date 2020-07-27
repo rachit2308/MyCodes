c1096d33f01e5df1d77c936b6eaf17456d6990f2
RACHIT_PAVILION\Rachit <rachitgupta230801@gmail.com>
2020-07-27 14:05:28 +0530

    count alpha number spaces

---
+++ countstring.py
@@ -0,0 +1,19 @@
+count1=0 #alphabets
+count2=0 #numbers
+count3=0 #spaces
+
+file=input("Enter the text for counting = ")
+
+for i in file:
+    if i.isalpha():
+        count1=count1+1
+    elif i.isdigit():
+        count2=count2+1
+    elif i.isspace():
+        count3=count3+1
+print("the text has alphabets= ")
+print(count1)
+print("the text has numbers= ")
+print(count2)
+print("the text has spaces= ")
+print(count3)
